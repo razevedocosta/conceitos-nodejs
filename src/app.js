7aaef897babbeabe09bacb8228855c126a0c6861
Rodrigo A. Costa <razevedocosta@gmail.com>
2020-11-01 23:31:20 -0400

    project modification and corrections

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid, validate: isUuid } = require('uuid');
+const { v4: uuid, validate: isUuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,81 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const likes = 0;
+
+  const repository  = {
+    id: uuid(),
+    title: title,
+    url: url,
+    techs: techs,
+    likes: likes,
+  }
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id == id
+  );
+
+  if (findRepositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  const repository = {
+    id: id,
+    title: title,
+    url: url,
+    techs: techs,
+    likes: repositories[findRepositoryIndex].likes,
+  };
+
+  repositories[findRepositoryIndex] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id == id
+  );
+
+  if (findRepositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists.' })
+  }; 
+
+  repositories.slice(findRepositoryIndex, 1);
+  return response.status(204).send();
+  
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findRepositoryIndex = repositories.findIndex(repository =>
+    repository.id == id
+  );
+
+  if (findRepositoryIndex < 0) {
+    return response.status(400).json({ error: 'Repository does not exists.' });
+  }
+
+  repositories[findRepositoryIndex].likes += 1;
+
+  return response.json(repositories[findRepositoryIndex]);
 });
 
 module.exports = app;
